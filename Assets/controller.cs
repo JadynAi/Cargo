90e16d826b725f74acd87fe0a2408336121fcac8
JadynAi <936745006@qq.com>
2022-12-17 16:49:14 +0800

    scripts
    add controller script for Player to control forward and horizontal turn, and for camera add script of Player follow, but still has a little bug

--- Assets/controller.cs
+++ Assets/controller.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -8,7 +9,11 @@ public class controller : MonoBehaviour
 
     public WheelCollider[] wheels = new WheelCollider[4];
 
+    public GameObject[] wheelMeshs = new GameObject[4];
+
     public float torque = 200;
+
+    public float steeringMax = 5;
 
     // Start is called before the first frame update
     void Start()
@@ -19,12 +24,42 @@ public class controller : MonoBehaviour
     // Update is called once per frame
     private void FixedUpdate()
     {
+        animateWheels();
         if(Input.GetKey(KeyCode.W))
         {
             for (int i = 0; i < wheels.Length; i++)
             {
                 wheels[i].motorTorque= torque;
             }
+        }
+
+        if(Input.GetAxis("Horizontal") != 0)
+        {
+            for (int i = 0; i < wheels.Length-2; i++)
+            {
+                wheels[i].steerAngle = Input.GetAxis("Horizontal") * steeringMax;
+                   
+            }
+        } else
+        {
+            for (int i = 0; i < wheels.Length - 2; i++)
+            {
+                wheels[i].steerAngle = 0;
+
+            }
+        }
+    }
+
+    private void animateWheels()
+    {
+        Vector3 wheekPosition = Vector3.zero;
+        Quaternion wheelRotation = Quaternion.identity;
+
+        for (int i = 0; i < 4; i++)
+        {
+            wheels[i].GetWorldPose(out wheekPosition, out wheelRotation);
+            wheelMeshs[i].transform.position = wheekPosition;
+            wheelMeshs[i].transform.rotation = wheelRotation;
         }
     }
 }
---
+++ Assets/scripts/cameraController.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraController : MonoBehaviour
+{
+
+    public GameObject Player;
+    public GameObject child;
+    public float speed;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+        child = Player.transform.Find("camera constraint").gameObject;
+    }
+
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        follow();
+    }
+
+    private void follow()
+    {
+        gameObject.transform.position = Vector3.Lerp(transform.position, child.transform.position, Time.deltaTime * speed);
+        gameObject.transform.LookAt(Player.transform.transform.position);
+    }
+}
+
+  
+ 
